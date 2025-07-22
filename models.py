347ff3f1917a01d0ad4c539c3283f5b551ce4198
Maarja Lehemets-Tihhanov <Maarja.Lehemets-Tihhanov@harku.ee>
2025-07-23 01:09:54 +0300

    Lisatud andmebaasimudelid

---
+++ models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Query(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    personal_id = db.Column(db.String(50), nullable=False)
+    result = db.Column(db.String(100))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
