8a8c75ee251b1b189af369fc76f9bef6acea6b95
Maarja Lehemets-Tihhanov <Maarja.Lehemets-Tihhanov@harku.ee>
2025-07-23 13:50:06 +0300

    muudetud

--- models.py
+++ models.py
@@ -2,22 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
 
-# db tuleb app.py failist (app.py -> from models import db)
+# Andmebaasi ühendus
 db = SQLAlchemy()
 
+# Kasutajate mudel
 class User(UserMixin, db.Model):
-    __tablename__ = 'user'
-
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-class Query(db.Model):
-    __tablename__ = 'query'
-
+# Päringute logi
+class QueryLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     personal_id = db.Column(db.String(20), nullable=False)
-    result = db.Column(db.String(100))
+    result = db.Column(db.String(100), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
