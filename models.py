653047e65fb950df5ff4fd15382d40f206da168a
Maarja Lehemets-Tihhanov <Maarja.Lehemets-Tihhanov@harku.ee>
2025-07-23 13:42:41 +0300

    Lisatud models.py: User ja Query tabelid

--- models.py
+++ models.py
@@ -2,19 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
 
-# Andmebaasi initsialiseerimine
+# db tuleb app.py failist (app.py -> from models import db)
 db = SQLAlchemy()
 
-# Kasutajate tabel
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-
-# Päringute logi
 class Query(db.Model):
+    __tablename__ = 'query'
+
     id = db.Column(db.Integer, primary_key=True)
     personal_id = db.Column(db.String(20), nullable=False)
     result = db.Column(db.String(100))
