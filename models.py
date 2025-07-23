cbbcbb392f0a2e1bd73df97bc5ec54bd6301c3bc
Maarja Lehemets-Tihhanov <Maarja.Lehemets-Tihhanov@harku.ee>
2025-07-23 13:37:59 +0300

    Lisatud models.py: User ja Query tabelid

--- models.py
+++ models.py
@@ -8,8 +8,10 @@ db = SQLAlchemy()
 # Kasutajate tabel
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
 
 # Päringute logi
 class Query(db.Model):
