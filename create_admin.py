76b64c5ff029184a44d4140ceaad6011e9688914
Maarja Lehemets-Tihhanov <Maarja.Lehemets-Tihhanov@harku.ee>
2025-07-23 13:59:59 +0300

    Uuendatud

--- create_admin.py
+++ create_admin.py
@@ -1,25 +1,19 @@
-import os
-from dotenv import load_dotenv
-from app import db, User, app
-from werkzeug.security import generate_password_hash
 from getpass import getpass
-
-# ✅ Laeme keskkonnamuutujad
-load_dotenv()
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+from werkzeug.security import generate_password_hash
+from app import app
+from models import db, User
 
 def create_admin():
     username = input("Sisesta admin kasutajanimi: ")
     password = getpass("Sisesta parool: ")
 
     with app.app_context():
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
+        if User.query.filter_by(username=username).first():
             print(f"⚠️ Kasutaja '{username}' on juba olemas.")
             return
 
         hashed_pw = generate_password_hash(password)
-        admin = User(username=username, password=hashed_pw)
+        admin = User(username=username, password=hashed_pw, is_admin=True)
         db.session.add(admin)
         db.session.commit()
         print(f"✅ Admin kasutaja '{username}' loodud.")
